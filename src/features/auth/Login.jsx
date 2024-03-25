4f48f3863f2fe1e00dfae785fb1dc889390c9435
PhuocTaii <tai.ngo1773@gmail.com>
2024-03-25 16:03:52 +0700

    Login UI

--- src/features/auth/Login.jsx
+++ src/features/auth/Login.jsx
@@ -1,9 +1,52 @@
+import React, {useState} from 'react'
+import { Input } from "@material-tailwind/react";
+import CustomButton from "../../components/CustomButton";
+import { FaRegUser, FaLock } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
 // Login page
 const Login = () => {
+  const [account, setAccount] = useState({username: '', password: ''});
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+
+  const handleChangeInfo = (e) => {
+    e.preventDefault();
+    const {name, value} = e.target;
+    setAccount({...account, [name]: value});
+  }
+
+  const handleSignin = (e) => {
+    e.preventDefault();
+    // loginUser(account, dispatch, navigate);
+  }
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <form className="flex flex-col items-center gap-8 w-full" onSubmit={(e) => handleSignin(e)}>
+      <p className='text-2xl font-medium'>Sign In</p>
+      <div className="w-full flex flex-col gap-3">
+        <Input 
+          icon={<FaRegUser />} 
+          label="Username"
+          onChange={(e) => handleChangeInfo(e)}
+          required
+          name="username"
+          value={account.username}
+        />
+        <Input 
+          icon={<FaLock />} 
+          label="Password"
+          type="password"
+          onChange={(e) => handleChangeInfo(e)}
+          required
+          name="password"
+          value={account.password}
+        />
+      </div>
+      <CustomButton label="Sign In" type='submit' />
+    </form>
   )
 }
 
